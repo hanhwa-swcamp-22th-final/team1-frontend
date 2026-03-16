50f93452e792117bb3ab66fd23cc95d0c3620fc1
playdata\playdata2 <gusgh075@gmail.com>
2026-03-16 15:42:49 +0900

    chore : 사이드바 메뉴 role별 분리

---
+++ src/components/layout/menus/whWorker.js
@@ -0,0 +1,23 @@
+/**
+ * WH Worker 사이드바 메뉴 구성
+ *
+ * TODO: WH Worker 담당 팀원이 작성
+ * ⚠️  WH Worker 화면은 태블릿 터치 UI 기준으로 개발할 것
+ *
+ * item 형태:
+ *   { name: ROUTE_NAMES.WH_WORKER_XXX, label: '메뉴 이름', icon: '아이콘' }
+ *
+ * 사용 가능한 ROUTE_NAMES (constants/routes.js 참고):
+ *   WH_WORKER_TASK_LIST, WH_WORKER_INSPECTION, WH_WORKER_PICKING, WH_WORKER_PACKING
+ */
+import { ROUTE_NAMES } from '@/constants'
+
+export const WH_WORKER_MENU_GROUPS = [
+  // TODO: 아래 주석을 참고하여 메뉴 그룹을 작성하세요
+  // {
+  //   label: '그룹명',
+  //   items: [
+  //     { name: ROUTE_NAMES.WH_WORKER_TASK_LIST, label: '작업 목록', icon: '≡' },
+  //   ],
+  // },
+]
