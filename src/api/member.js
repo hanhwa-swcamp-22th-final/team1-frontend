65bb61ad0eeedd0d09abb8f2f7bf2ae47a516753
ikth-kim <ikth.kim@gmail.com>
2026-03-16 15:27:16 +0900

    feat: 공통 로그인 페이지 구현

--- src/api/member.js
+++ src/api/member.js
@@ -1,1 +1,10 @@
-// TODO: 팀원 담당
+import instance from './instance.js'
+
+/**
+ * 로그인
+ * @param {{ email: string, password: string }} payload
+ * @returns {Promise<AxiosResponse>}
+ */
+export async function login(payload) {
+  return instance.post('/auth/login', payload)
+}
